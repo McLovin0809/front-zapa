269b226d517db1e5a25e8565405c3bb8a3d54d17
McLovin0809 <m.belmar@duocuc.cl>
2025-11-24 00:22:10 -0300

    aaaa

--- src/data/navbarPublicLinks.js
+++ src/data/navbarPublicLinks.js
@@ -2,7 +2,7 @@
 export const PublicLinks = (user, totalItems) => {
   const links = [
     { label: "Inicio", to: "/" },
-    { label: "Productos", to: "/productos" },
+    { label: "Productos", to: "/ProductosUser" },
     { label: "Sobre Nosotros", to: "/about" },
     { label: user?.email ? "Perfil" : "Iniciar sesión", to: user?.email ? "/perfil" : "/login" },
     { label: `🛒 Carrito (${totalItems})`, to: "/carrito" },
