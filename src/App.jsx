bd9da1c2d6c81632800a65b9513ce455e60e3e3b
McLovin0809 <m.belmar@duocuc.cl>
2025-11-23 23:15:57 -0300

    r

--- src/App.jsx
+++ src/App.jsx
@@ -48,5 +48,4 @@ function Layout() {
 function App() {
   return <Layout />;
 }
-
 export default App;
--- src/data/navbarPublicLinks.js
+++ src/data/navbarPublicLinks.js
@@ -2,7 +2,6 @@ export const publicLinks = [
   { label: "Inicio", to: "/" },
   { label: "Productos", to: "/ProductosUser" },
   { label: "Sobre Nosotros", to: "/about" }
-
 ];
 
 export default publicLinks;
