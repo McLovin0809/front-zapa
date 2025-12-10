64edbe484c047bff610612403528d7e218645cf5
Javier nc. <ja.niloc@duocuc.cl>
2025-12-10 01:27:45 -0300

    201funcionaxfa

--- src/services/ProductoService.jsx
+++ src/services/ProductoService.jsx
@@ -3,54 +3,47 @@ import axios from 'axios';
 
 const BASE_URL = 'https://backend-zapa.onrender.com/api/productos';
 
-const authHeader = () => {
-  const token = localStorage.getItem("token");
-  return {
-    Authorization: `Bearer ${token}`
-  };
-};
-
 class ProductoService {
+    getAllProductos() {
+        return axios.get(BASE_URL);
+    }
 
-  getAllProductos() {
-    return axios.get(BASE_URL, { headers: authHeader() });
-  }
+    getProductoById(id) {
+        return axios.get(`${BASE_URL}/${id}`);
+    }
 
-  getProductoById(id) {
-    return axios.get(`${BASE_URL}/${id}`, { headers: authHeader() });
-  }
+    createProducto(producto) {
+        return axios.post(BASE_URL, producto);
+    }
 
-  createProducto(producto) {
-    return axios.post(BASE_URL, producto, { headers: authHeader() });
-  }
+    updateProducto(id, producto) {
+        return axios.put(`${BASE_URL}/${id}`, producto);
+    }
 
-  updateProducto(id, producto) {
-    return axios.put(`${BASE_URL}/${id}`, producto, { headers: authHeader() });
-  }
+    deleteProducto(id) {
+        return axios.delete(`${BASE_URL}/${id}`);
+    }
 
-  deleteProducto(id) {
-    return axios.delete(`${BASE_URL}/${id}`, { headers: authHeader() });
-  }
+    //metodos personalizados
+    buscarPorMarca(nombreMarca) {
+        return axios.get(`${BASE_URL}/marca/${nombreMarca}`);
+    }
 
-  buscarPorMarca(nombreMarca) {
-    return axios.get(`${BASE_URL}/marca/${nombreMarca}`, { headers: authHeader() });
-  }
+    buscarPorRangoPrecio(min, max) {
+        return axios.get(`${BASE_URL}/precio?min=${min}&max=${max}`);
+    }
 
-  buscarPorRangoPrecio(min, max) {
-    return axios.get(`${BASE_URL}/precio?min=${min}&max=${max}`, { headers: authHeader() });
-  }
+    buscarPorCategoria(categoria) {
+        return axios.get(`${BASE_URL}/categoria/${categoria}`);
+    }
 
-  buscarPorCategoria(categoria) {
-    return axios.get(`${BASE_URL}/categoria/${categoria}`, { headers: authHeader() });
-  }
+    buscarPorGenero(genero) {
+        return axios.get(`${BASE_URL}/genero/${genero}`);
+    }
 
-  buscarPorGenero(genero) {
-    return axios.get(`${BASE_URL}/genero/${genero}`, { headers: authHeader() });
-  }
-
-  buscarConDescuento(descuento) {
-    return axios.get(`${BASE_URL}/descuento/${descuento}`, { headers: authHeader() });
-  }
+    buscarConDescuento(descuento) {
+        return axios.get(`${BASE_URL}/descuento/${descuento}`);
+    }
 }
 
 export default new ProductoService();
